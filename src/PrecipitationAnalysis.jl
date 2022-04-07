b4fbac8b6f246f621d187a175fb641035447fbb6
Felipe Flores <lffm.fismat@gmail.com>
2022-04-06 22:26:30 -0600

    exporting important functions

--- src/PrecipitationAnalysis.jl
+++ src/PrecipitationAnalysis.jl
@@ -4,7 +4,8 @@ using CSV, Serialization, Dates, Geodesy, DataStructures, Statistics, StructArra
 using PolygonOps
 import Base: getindex, eltype
 
-export STATIONS, GEOMETRIES, DEPARTAMENTOS
+export STATIONS, GEOMETRIES, DEPARTAMENTOS, TIME_DENSITIES, CONTEO_DE_COINCIDENCIAS
+export count_date_matching_stations, dates_with_matches_over
 export WeatherStation
 
 include("constants.jl")
